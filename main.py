4f9fcb7b74cc660d6c723cf826695805a21619e8
Victor Finel <victor.finel.pro@gmail.com>
2025-12-25 20:01:10 +0100

    fix bug -> log stations also when using jog wheel

--- main.py
+++ main.py
@@ -275,6 +275,14 @@ def play_station_and_stop_noise(url):  # url_list[jog]
     global radio_player
     radio_player = play_radio(url)
     scheduler.attach_timer(pause_noise, 2)
+
+    log_radio(location=location_name, 
+            latitude=latitude, 
+            longitude=longitude,
+            station=stations_list[jog], 
+            url=url_list[jog],
+            )
+    
     return radio_player
 
 
@@ -373,9 +381,7 @@ while True:
             display_thread.clear()
             update_display()
 
-            radio_player = play_station_and_stop_noise(url_list[jog])
-            log_radio(location_name, latitude, longitude, stations_list[jog], url_list[jog])
-            
+            radio_player = play_station_and_stop_noise(url_list[jog])            
 
         # Exit back to tuning state if latch has 'come unstuck'
         elif not encoders_thread.is_latched():
